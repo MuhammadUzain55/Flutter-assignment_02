471f17a16ec87e4217c1bb31f2b481611ffc4641
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-13 18:15:33 +0500

    assignment Q.3

---
+++ ASSIGNMENT_02/Question3.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Q.3: Create a list of days and remove one by one from the end of list.
+
+  List days = [
+    ["Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"]
+  ];
+  print(days.removeAt(0)[2]);
+}
