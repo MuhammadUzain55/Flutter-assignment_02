fa9e4aef7617d89ba7c3b09dd6243cb79044e6cc
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-13 18:04:15 +0500

    assignment Q.1

---
+++ ASSIGNMENT_02/Question1.dart
@@ -0,0 +1,7 @@
+void main() {
+  // Q.1: Create a list of names and print all names using list.
+
+  
+  List names = ["Shehroz", "Faraz", "Sameer", "Murtaza", "Haneef", "Salman"];
+  print(names);
+}
