df62732d9ec3a873b7c39abb7dd9b479e637b9b8
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-16 18:35:22 +0500

    assignment Q.7

---
+++ ASSIGNMENT_02/Question7.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Q.7: Map<string, double> expenses = {
+  //        "sun": 3000.0,
+  //        "mon": 3000.0,
+  //        "tue": 3234.0,
+  // };
+  // check if "fri" exist in expenses; if exist change it's value to 5000.0 
+  // otherwise add "fri" to expenses and set its value to 5000.0 then print expenses.
+}
