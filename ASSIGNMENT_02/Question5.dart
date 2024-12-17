4301152eadf6501efc9539d094e85ace95a784c0
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-17 14:59:45 +0500

    Updates Q.5

--- ASSIGNMENT_02/Question5.dart
+++ ASSIGNMENT_02/Question5.dart
@@ -1,9 +1,8 @@
 void main() {
-
   // Q.5: Create a map with name, phone keys and store some values to it.
-  // Use where to find all keys that have length 4. 
-           
+  // Use where to find all keys that have length 4.
 
-
- 
+  Map phones = {"Samsung": 32000, "iphone": 80000, "itel": 56000};
+  var phone = phones.keys.where((key) => key.length <= 4).toList();
+  print(phone);
 }
