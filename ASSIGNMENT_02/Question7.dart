c3783b8bfe8a378564ae4774b44b1f730eca6996
MuhammadUzain55 <uzainsheikh55@gmail.com>
2024-12-17 15:08:11 +0500

    Updates

--- ASSIGNMENT_02/Question7.dart
+++ ASSIGNMENT_02/Question7.dart
@@ -4,6 +4,14 @@ void main() {
   //        "mon": 3000.0,
   //        "tue": 3234.0,
   // };
-  // check if "fri" exist in expenses; if exist change it's value to 5000.0 
+  // check if "fri" exist in expenses; if exist change it's value to 5000.0
   // otherwise add "fri" to expenses and set its value to 5000.0 then print expenses.
+
+  Map<String, double> expenses = {
+    "sun": 3000.0,
+    "mon": 3000.0,
+    "tue": 3234.0,
+  };
+  expenses["fri"] = 5000.0;
+  print(expenses);
 }
